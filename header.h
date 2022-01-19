40e8934a16c47a7623bb03f607de1ee22f8103d5
iyoshiha <study.e.jp@gmail.com>
2022-01-20 03:31:58 +0900

    header

--- header.h
+++ header.h
@@ -5,3 +5,6 @@ typedef struct	s_partition
 	int x;
 }				t_partition;
 
+void	swap_ary_elem(int *a_ptr, int *b_ptr);
+int	partition(int *ary, ssize_t i_head, ssize_t i_tail);
+int quick_sort(int *ary, ssize_t i_head, ssize_t i_tail);
