d0ecfe4170f63f097d30e687328c92dab7eb5f69
iyoshiha <study.e.jp@gmail.com>
2022-01-20 13:49:44 +0900

    optimized

--- quick_sort.c
+++ quick_sort.c
@@ -3,23 +3,9 @@
 void	quick_sort(int *ary, ssize_t i_head, ssize_t i_tail)
 {
 	ssize_t i_border;
-	static int ch;
 
-	/*
-	puts("**************");
-	printf("%d times: head: %d tail: %d\n",ch, i_head, i_tail);
-	for (int i = 0; i < 10; i++)
-	{
-		printf("ary[%d] = %d\n", i,  ary[i]);
-	}
-	puts("**************");
-	ch++;
-	*/
 	if (i_head >= i_tail)
-	{
-	// printf("%d times: head: %d tail: %d\n",ch, i_head, i_tail);
 		return ;
-	}
 	i_border = partition(ary, i_head, i_tail);
 	quick_sort(ary, i_head, i_border - 1);
 	quick_sort(ary, i_border + 1, i_tail);
